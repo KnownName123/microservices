74e2f97268cce95ecdf8156b84e9dec803ad28c9
Tamiriran Viktor <94015359+KnownName123@users.noreply.github.com>
2025-08-29 18:46:58 +0300

    Delete Song-microservice/resource-microservice directory

--- Song-microservice/resource-microservice/src/main/java/com/example/resource/microservice/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.example.resource.microservice.exception;
-
-public class BadRequestException extends RuntimeException{
-    public BadRequestException(String message) {
-            super(message);
-        }
-}
