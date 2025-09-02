2916d735aa4e114edc9c660b7fa63b349b49d7da
Tamiriran Viktor <94015359+KnownName123@users.noreply.github.com>
2025-09-02 20:24:57 +0300

    Delete resource-microservice directory

--- resource-microservice/src/main/java/com/example/resource/microservice/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.resource.microservice.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
