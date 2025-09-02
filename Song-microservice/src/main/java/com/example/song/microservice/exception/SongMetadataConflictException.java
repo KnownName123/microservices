b555669e597a88ca3f84106898671ae0a34ac3c3
Tamiriran Viktor <94015359+KnownName123@users.noreply.github.com>
2025-09-02 20:24:46 +0300

    Delete Song-microservice directory

--- Song-microservice/src/main/java/com/example/song/microservice/exception/SongMetadataConflictException.java
+++
@@ -1,7 +0,0 @@
-package com.example.song.microservice.exception;
-
-public class SongMetadataConflictException extends RuntimeException {
-    public SongMetadataConflictException(String message) {
-        super(message);
-    }
-}
--- Song-microservice/src/main/java/com/example/song/microservice/exception/SongMetadataNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.song.microservice.exception;
-
-public class SongMetadataNotFoundException extends RuntimeException{
-    public SongMetadataNotFoundException(String message) {
-        super(message);
-    }
-}
