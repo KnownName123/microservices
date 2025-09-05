4cf6414cbffd87ccd975bed9c479a4b0263fc122
Tamiriran Viktor <94015359+KnownName123@users.noreply.github.com>
2025-09-05 18:54:29 +0300

    Delete resource-service directory

--- resource-service/src/main/java/com/tamirian/resource/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.tamirian.resource.exception;
-
-public class BadRequestException extends RuntimeException{
-    public BadRequestException(String message) {
-            super(message);
-        }
-}
