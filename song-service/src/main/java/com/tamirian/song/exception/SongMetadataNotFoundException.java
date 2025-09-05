521683f055181358f3772c7897242085800d4ec6
Tamiriran Viktor <94015359+KnownName123@users.noreply.github.com>
2025-09-05 18:54:40 +0300

    Delete song-service directory

--- song-service/src/main/java/com/tamirian/song/exception/SongMetadataNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.tamirian.song.exception;
-
-public class SongMetadataNotFoundException extends RuntimeException{
-    public SongMetadataNotFoundException(String message) {
-        super(message);
-    }
-}
